45cb0c82de4ead28eb1784d5c2f71c3e33954993
Liu-shengnan <1531653056@qq.com>
2020-11-12 10:09:20 +0800

    主干冲突添加

--- mavenTest/src/main/java/com/shengnan/maven/Hello.java
+++ mavenTest/src/main/java/com/shengnan/maven/Hello.java
@@ -13,6 +13,7 @@ public class Hello {
             System.out.println("更新4");
             System.out.println("创建分支");
             System.out.println("分支首次添加");
+            System.out.println("主干冲突添加");
             return "Hello "+name+"!";
 
         }
