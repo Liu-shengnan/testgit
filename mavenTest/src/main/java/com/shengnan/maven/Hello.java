22763ab36206f7742b4c5fadfc87cce7654f7140
Liu-shengnan <1531653056@qq.com>
2020-11-12 09:42:25 +0800

    首次提交

---
+++ mavenTest/src/main/java/com/shengnan/maven/Hello.java
@@ -0,0 +1,14 @@
+package com.shengnan.maven;
+
+import java.sql.SQLOutput;
+
+public class Hello {
+
+
+        public String sayHello(String name){
+
+
+            return "Hello "+name+"!";
+        }
+
+}
