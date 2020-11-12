6a2047e6928057f86329c7e090aaacb066480fef
Liu-shengnan <1531653056@qq.com>
2020-11-12 10:02:21 +0800

    二次

--- mavenTest/src/main/java/com/shengnan/maven/Hello.java
+++ mavenTest/src/main/java/com/shengnan/maven/Hello.java
@@ -7,8 +7,12 @@ public class Hello {
 
         public String sayHello(String name){
 
-
+            System.out.println("更新1");
+            System.out.println("更新2");
+            System.out.println("更新3");
+            System.out.println("更新4");
             return "Hello "+name+"!";
+
         }
 
 }
